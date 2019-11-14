4fde2c10b5413eba40ae77fca9fc9bef6152d32c
Tyisha <tlwilliams895@gmail.com>
2019-11-14 14:03:50 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/controllers/ListController.java
@@ -0,0 +1,67 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.JobData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+//To see the values of columns
+/**
+ * Created by LaunchCode
+ */
+@Controller
+@RequestMapping(value = "list")
+public class ListController {
+
+    static HashMap<String, String> columnChoices = new HashMap<>();
+
+    public ListController () {
+        columnChoices.put("core competency", "Skill");
+        columnChoices.put("employer", "Employer");
+        columnChoices.put("location", "Location");
+        columnChoices.put("position type", "Position Type");
+        columnChoices.put("all", "All");
+    }
+    //this is the index
+    @RequestMapping(value = "")
+    public String list(Model model) {
+
+        model.addAttribute("columns", columnChoices);
+
+        return "list";
+    }
+//Uses query parameters passed in the column to figure which values to fetch from JobData
+    @RequestMapping(value = "values")
+    public String listColumnValues(Model model, @RequestParam String column) {
+
+        if (column.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+            model.addAttribute("jobs", jobs);
+            return "list-jobs";
+        } else {
+            ArrayList<String> items = JobData.findAll(column);
+            model.addAttribute("title", "All " + columnChoices.get(column) + " Values");
+            model.addAttribute("column", column);
+            model.addAttribute("items", items);
+            return "list-column";
+        }
+
+    }
+
+//Model is because is communicating with the JobData.csv file to the Controller
+//This method only displays matching specific jobs (matching a value in a specific column)
+    @RequestMapping(value = "jobs")
+    public String listJobsByColumnAndValue(Model model,
+            @RequestParam String column, @RequestParam String value) {
+
+        ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(column, value);
+        model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
+        model.addAttribute("jobs", jobs);
+
+        return "list-jobs";
+    }
+}
---
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -0,0 +1,36 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.JobData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+@RequestMapping("search")
+public class SearchController {
+
+    @RequestMapping(value = "")
+    public String search(Model model) {
+        model.addAttribute("columns", ListController.columnChoices);
+        return "search";
+    }
+
+    @RequestMapping(value = "results")
+    public String search(Model model,
+                         @RequestParam String searchType,
+                         @RequestParam String searchTerm) {
+        ArrayList<HashMap<String, String>> results = JobData.findByColumnAndValue(searchType, searchTerm);
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("results", results);
+        return "search";
+    }
+
+}
